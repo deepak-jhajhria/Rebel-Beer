b16915e1ace112a63b2b6533fd6909605eef95ad
mayankpcp7 <jariwalmayank123@gmail.com>
2024-07-02 11:09:00 +0530

    merge code

---
+++ assets/js/nav.js
@@ -0,0 +1,6 @@
+// ====================NAVBAR JS=====================
+function openNav() {
+  document.getElementById("navbar").classList.toggle("left-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross");
+}
